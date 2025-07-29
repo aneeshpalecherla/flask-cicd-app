a896246c30418b8c52350d9a097e621984472639
aneeshpalecherla <aneesh.palecherla@zapcg.com>
2025-07-29 13:52:26 -0500

    Correctly place Terraform files in 'terraform' directory

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name = "allow_ssh_http_terraform"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "flask_app" {
+  ami                    = "ami-0cd582ee8a22cc7be" # Ubuntu 22.04 LTS AMI
+  instance_type          = "t2.micro"
+  key_name               = "sunny69"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+
+  tags = {
+    Name = "FlaskAppServer"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+apt update -y
+apt install docker.io -y
+systemctl start docker
+systemctl enable docker
+EOF
+}
+output "public_ip" {
+  value = aws_instance.my_instance.public_ip
+}
